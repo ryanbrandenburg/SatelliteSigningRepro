7e52a11953e10a93b8e0418d9bcd091b39c64934
Ryan Brandenburg <rybrande@microsoft.com>
2017-02-01 15:44:57 -0800

    Satelitte assembly repro

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MvcApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Do nothing!");
+        }
+    }
+}
